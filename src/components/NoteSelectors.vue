fd94e9f8b4748d0062a9bdd826cedca651b05fce
joycejewell <joyceroop@gmail.com>
2022-08-03 19:57:56 -0600

    parent and child notes component using v-for

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,18 +1,24 @@
+<script>
+import NoteSelector from "./NoteSelector";
+export default {
+  components: {
+    NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a test test", timestamp: Date.now() },
+      ],
+    };
+  },
+};
+</script>
+
 <template>
-  <div class="note-container">
-    <div class="note-selectors">
-      <div class="note-selector active">
-        <p class="note-selector-title">First note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Second note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-      <div class="note-selector">
-        <p class="note-selector-title">Third note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
-      </div>
-    </div>
+  <div class="note-selectors">
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="notes" />
   </div>
 </template>
