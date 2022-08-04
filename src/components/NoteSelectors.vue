b438d8e8ef01dab96adb643b1c1d3cbb3c45b3fd
joycejewell <joyceroop@gmail.com>
2022-08-03 20:06:22 -0600

    remove the s from notes to note in v-bind

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -10,7 +10,7 @@ export default {
         { id: 1, body: "This is a first test", timestamp: Date.now() },
         { id: 2, body: "This is a second test", timestamp: Date.now() },
         { id: 3, body: "This is a third test", timestamp: Date.now() },
-        { id: 4, body: "This is a test test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
       ],
     };
   },
@@ -19,6 +19,6 @@ export default {
 
 <template>
   <div class="note-selectors">
-    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="notes" />
+    <NoteSelector v-for="note in notes" v-bind:key="note.id" v-bind:note="note" />
   </div>
 </template>
